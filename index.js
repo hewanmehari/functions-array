3a7311d6b1e6da37f184f096f3de4070d2f98c5f
hewan <hewaanmehari@gmail.com>
2025-04-10 14:47:13 +0300

    did functions and array

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
-function getStringLengths(arr) {
-    return arr.map(str => str.length);
-  }
-  
 
   
-  function getEvenNumbers(numbers) {
-    return numbers.filter(num => num % 2 === 0);
-  }
-  
-  function topStudents(students) {
-    return students
-      .filter(student => student.score > 75)
-      .map(student => student.name);
-  }
-  
-  function squareNumbers(numbers) {
-    return numbers.map(num => num * num);
-  }
+    function getStringLengths(strings) {
+    return strings.map(string => string.length);
+    }
+    console.log(getStringLengths(["hewan", "loves", "photography"])); 
+    
+    function filterEvenNumbers(numbers) {
+        return numbers.filter(number => number % 2 === 0);
+    }
 
-  function getUppercaseStrings(arr) {
-    return arr
-      .filter(item => typeof item === 'string')
-      .map(str => str.toUpperCase());
-  }
-  
-  
+    console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+    
+    function getTopStudents(students) {
+        return students
+            .filter(student => student.score > 75)
+            .map(student => student.name);
+    }
+    const students = [
+        { name: "Melat", score: 80 },
+        { name: "Bethel", score: 70 },
+        { name: "Nebyat", score: 90 }
+    ];
+        console.log(getTopStudents(students)); 
+
+    
+
+    function squareNumbers(numbers) {
+        return numbers.map(number => number * number);
+    }
+
+    console.log(squareNumbers([3, 5, 9, 7])); 
+
+    function getUppercaseStrings(elements) {
+        return elements
+            .filter(element => typeof element === "string")
+            .map(string => string.toUpperCase());
+    }
+    console.log(getUppercaseStrings(["people", 143, "help", true, "world"]));     
+ 
+
+
